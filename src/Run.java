f866f4a346b1d2be112c29c2a1c765ab89cd3a4c
yoyojackson <jason7830@gmail.com>
2019-08-19 21:05:39 +0800

    _

--- src/Run.java
+++ src/Run.java
@@ -6,11 +6,13 @@ import com.sun.jna.platform.win32.WinDef.*;
 import com.sun.jna.win32.StdCallLibrary;
 import java.awt.*;
 import java.awt.event.*;
+import com.sun.jna.platform.win32.BaseTSD;
 
 
 public class Run {
 	public static void main(String[] args)
 	{
+		/*
 		Frame f = new Frame("test");
 		TextArea ta = new TextArea("");
 		ta.setBounds(50,50,50,50);
@@ -24,32 +26,55 @@ public class Run {
 				System.exit(0);
 			}
 		});
-	
+		*/
+		user32.LPINPUT input = new user32.LPINPUT();
+		user32.LPINPUT.DUMMYUNIONNAME dummy = new user32.LPINPUT.DUMMYUNIONNAME();
+		input.type = new WinUser.DWORD(1);
+		dummy.ki.time = new WinUser.DWORD(0);
+		dummy.ki.dwExtraInfo = new BaseTSD.ULONG_PTR(0);
+		dummy.ki.wScan = new WinUser.WORD(0);
+		dummy.ki.dwFlags = new WinUser.DWORD(0);
+		dummy.ki.wVk = new WinUser.WORD(0x41);
+		user32.INSTANCE.SendInput(1, input , input.size());
+		System.out.println("TEST");
 	}
 	
-	public static class MOUSEUNPUT extends Structure{
-		long dx;
-		long dy;
-		int mouseData;
-		int dwFlags;
-		int time;
-		Pointer 
-		
-	}
-	
-	public static class tagINPUT extends Structure{
-		int type;
-		public static class DUMMYUNIONNAME extends Union{
-			
-		}
-		
-	}
 	
 	public interface user32 extends StdCallLibrary{
 		user32 INSTANCE = (user32)Native.loadLibrary("user32",user32.class);
+		public int SendInput(long cInputs,LPINPUT pInputs, int cbsize);
+		public static class MOUSEINPUT extends Structure{
+			public WinUser.LONG dx;
+			public WinUser.LONG dy;
+			public WinUser.DWORD mouseData;
+			public WinUser.DWORD dwFlags;
+			public WinUser.DWORD time;
+			public BaseTSD.ULONG_PTR dwExtraInfo;
+			
+		}
 		
-		public int SendInput(long cInputs, int cbsize);
+		public static class KEYBDINPUT extends Structure{
+			public WinUser.WORD wVk;
+			public WinUser.WORD wScan;
+			public WinUser.DWORD dwFlags;
+			public WinUser.DWORD time;
+			public BaseTSD.ULONG_PTR dwExtraInfo;
+		}
 		
+		public static class HARDWAREINPUT extends Structure{
+			public WinUser.DWORD Msg;
+			public WinUser.WORD wParamL;
+			public WinUser.WORD wParamH;
+		}
+		
+		public static class LPINPUT extends Structure{
+			public WinUser.DWORD type;
+			public static class DUMMYUNIONNAME extends Union{
+				public MOUSEINPUT mi;
+				public KEYBDINPUT ki;
+				public HARDWAREINPUT hi;
+			}
+		}
 	}
 	
 }
