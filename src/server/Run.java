1b31e00676be0c563b43b92db2e968def7a6be46
yoyojackson <jason7830@gmail.com>
2019-08-20 16:01:09 +0800

    -

--- src/server/Run.java
+++ src/server/Run.java
@@ -1,3 +1,5 @@
+package server;
+
 import com.sun.jna.Native;
 import com.sun.jna.Union;
 import com.sun.jna.Structure;
@@ -9,9 +11,9 @@ import java.awt.event.*;
 import java.lang.reflect.Field;
 import java.util.*;
 import java.util.List;
-
 import com.sun.jna.platform.win32.BaseTSD;
 
+import server.win32.WinUser.*;
 
 public class Run {
 	public static void main(String[] args) throws InterruptedException
@@ -56,75 +58,6 @@ public class Run {
 			System.out.println("SLEPT");
 		}
 		//System.out.println("TEST");
-	}
-	
-	public static class MOUSEINPUT extends Structure{
-		public MOUSEINPUT() {}
-		public BaseTSD.ULONG_PTR dwExtraInfo;
-		public WinDef.DWORD dwFlags;
-		public WinDef.LONG dx;
-		public WinDef.LONG dy;
-		public WinDef.DWORD mouseData;
-		public WinDef.DWORD time;
-		protected List getFieldOrder() {
-			return Run.getFieldOrder(this.getClass());
-		}
-		
-	}
-	
-	public static class KEYBDINPUT extends Structure{
-		public KEYBDINPUT() {}
-		public WinDef.WORD wVk;
-		public WinDef.WORD wScan;
-		public WinDef.DWORD dwFlags;
-		public WinDef.DWORD time;
-		public BaseTSD.ULONG_PTR dwExtraInfo;
-		protected List getFieldOrder() {
-			return Run.getFieldOrder(this.getClass());
-		}
-	}
-	
-	public static class HARDWAREINPUT extends Structure{
-		public HARDWAREINPUT() {}
-		public WinDef.DWORD Msg;
-		public WinDef.WORD wParamL;
-		public WinDef.WORD wParamH;
-		protected List getFieldOrder() {
-			return Run.getFieldOrder(this.getClass());
-		}
-	}
-	
-	public static class INPUT extends Structure{
-		public INPUT() {}
-		public WinDef.DWORD type;
-		public DUMMYUNIONNAME dummy = new DUMMYUNIONNAME();
-		
-		public static class DUMMYUNIONNAME extends Union{
-			public DUMMYUNIONNAME() {
-				
-			}
-			
-			public MOUSEINPUT mi;
-			public KEYBDINPUT ki;
-			public HARDWAREINPUT hi;
-			protected List getFieldOrder() {
-				return Run.getFieldOrder(this.getClass());
-			}
-		}
-		protected List getFieldOrder() {
-			return Run.getFieldOrder(this.getClass());
-		}
-	}
-	
-	private static List getFieldOrder(Class<?> c)
-	{
-		List<String> names = new ArrayList<String>();
-		Field[] fields = c.getDeclaredFields();
-		for(Field f : fields)
-		{
-			names.add(f.getName());
-		}
-		return names;
 	}
 	
 	public interface user32 extends StdCallLibrary{
---
+++ src/server/win32/WinUser.java
@@ -0,0 +1,84 @@
+package server.win32;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.sun.jna.Native;
+import com.sun.jna.Structure;
+import com.sun.jna.Union;
+import com.sun.jna.platform.win32.BaseTSD;
+import com.sun.jna.platform.win32.WinDef;
+
+import server.Run;
+
+
+public interface WinUser {
+	
+	public static List getFieldOrder(Class<?> c)
+	{
+		List<String> names = new ArrayList<String>();
+		Field[] fields = c.getDeclaredFields();
+		for(Field f : fields)
+		{
+			names.add(f.getName());
+		}
+		return names;
+	}
+	
+	public static class MOUSEINPUT extends Structure{
+		public MOUSEINPUT() {}
+		public BaseTSD.ULONG_PTR dwExtraInfo;
+		public WinDef.DWORD dwFlags;
+		public WinDef.LONG dx;
+		public WinDef.LONG dy;
+		public WinDef.DWORD mouseData;
+		public WinDef.DWORD time;
+		protected List getFieldOrder() {
+			return WinUser.getFieldOrder(this.getClass());
+		}
+		
+	}
+	
+	public static class KEYBDINPUT extends Structure{
+		public KEYBDINPUT() {}
+		public WinDef.WORD wVk;
+		public WinDef.WORD wScan;
+		public WinDef.DWORD dwFlags;
+		public WinDef.DWORD time;
+		public BaseTSD.ULONG_PTR dwExtraInfo;
+		protected List getFieldOrder() {
+			return WinUser.getFieldOrder(this.getClass());
+		}
+	}
+	
+	public static class HARDWAREINPUT extends Structure{
+		public HARDWAREINPUT() {}
+		public WinDef.DWORD Msg;
+		public WinDef.WORD wParamL;
+		public WinDef.WORD wParamH;
+		protected List getFieldOrder() {
+			return WinUser.getFieldOrder(this.getClass());
+		}
+	}
+	
+	public static class INPUT extends Structure {
+		public INPUT() {}
+		public WinDef.DWORD type;
+		public DUMMYUNIONNAME dummy = new DUMMYUNIONNAME();
+		
+		public static class DUMMYUNIONNAME extends Union{
+			//public DUMMYUNIONNAME() {}
+			
+			public MOUSEINPUT mi;
+			public KEYBDINPUT ki;
+			public HARDWAREINPUT hi;
+			protected List getFieldOrder() {
+				return WinUser.getFieldOrder(this.getClass());
+			}
+		}
+		protected List getFieldOrder() {
+			return WinUser.getFieldOrder(this.getClass());
+		}
+	}
+}
